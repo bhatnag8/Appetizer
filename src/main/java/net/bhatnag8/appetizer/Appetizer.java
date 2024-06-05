be08c3054367564ffd648cd86638cd0275930498
Arryan Bhatnagar <bhatnag8@purdue.edu>
2024-06-06 02:03:58 +0530

    added new villager trade
    - villager now accepts custom trades

--- src/main/java/net/bhatnag8/appetizer/Appetizer.java
+++ src/main/java/net/bhatnag8/appetizer/Appetizer.java
@@ -1,5 +1,6 @@
 package net.bhatnag8.appetizer;
 
+import net.bhatnag8.appetizer.util.ModCustomTrades;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -17,7 +18,7 @@ public class Appetizer implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		ModCustomTrades.registerCustomTrades();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/net/bhatnag8/appetizer/util/ModCustomTrades.java
@@ -0,0 +1,27 @@
+package net.bhatnag8.appetizer.util;
+
+import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
+import net.minecraft.enchantment.EnchantmentLevelEntry;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.EnchantedBookItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradedItem;
+import net.minecraft.village.VillagerProfession;
+
+public class ModCustomTrades {
+
+    public static void registerCustomTrades() {
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.FARMER, 5, factories -> {
+
+
+            factories.add((entity, random) -> new TradeOffer(
+                   new TradedItem(new ItemStack(Items.EMERALD).getItem(), 32),
+                    EnchantedBookItem.forEnchantment( new EnchantmentLevelEntry(Enchantments.FORTUNE, 3)),
+                    1, 12, 0.075f)
+            );
+        });
+    }
+}
