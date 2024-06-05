877099ab8572d42710570c752efb731fa08402f6
Arryan Bhatnagar <bhatnag8@purdue.edu>
2024-06-06 01:06:34 +0530

    Initial Commit

---
+++ src/main/java/net/bhatnag8/appetizer/AppetizerClient.java
@@ -0,0 +1,10 @@
+package net.bhatnag8.appetizer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AppetizerClient  implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/bhatnag8/appetizer/AppetizerDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bhatnag8.appetizer;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AppetizerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
